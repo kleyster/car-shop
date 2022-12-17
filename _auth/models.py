77c7020dd93a3e6e7f8aee534597b31f3cef1fce
Adilzhan Aubakir <aubakirof@gmail.com>
2022-12-17 16:31:47 +0600

    fix

--- _auth/models.py
+++ _auth/models.py
@@ -1,7 +1,7 @@
 import uuid
 
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
 from django.contrib.auth.hashers import make_password
 from django.utils.translation import gettext_lazy as _
 
@@ -38,9 +38,8 @@ class CustomUserManager(UserManager):
         return self._create_user(email, password, **extra_fields)
 
 
-class Users(AbstractBaseUser):
+class Users(AbstractBaseUser, PermissionsMixin):
 
-    id = models.BigAutoField(primary_key=True)
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=60, null=True)
     last_name = models.CharField(max_length=60, null=True)

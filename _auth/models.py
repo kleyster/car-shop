1148dba964a30f9071616366c52ce86c0e94eb52
Adilzhan Aubakir <aubakirof@gmail.com>
2022-12-17 15:42:59 +0600

    fix

--- _auth/models.py
+++ _auth/models.py
@@ -1,5 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, User, UserManager
+from django.contrib.auth.hashers import make_password
+
+
+class CustomUserManager(UserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+                Create and save a user with the given username, email, and password.
+                """
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class Users(AbstractBaseUser):
@@ -13,4 +46,4 @@ class Users(AbstractBaseUser):
 
     REQUIRED_FIELDS = []
     USERNAME_FIELD = "email"
-    objects = UserManager()
+    objects = CustomUserManager()

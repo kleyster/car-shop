4d49eb8787417f6b082717f7810ffc59997ceca8
Adilzhan Aubakir <aubakirof@gmail.com>
2023-02-14 13:10:08 +0600

    update

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
+from orders.models import ProductApplications
 
 
 class ApplicationFormSerializer(serializers.ModelSerializer):
 
     class Meta:
+        model = ProductApplications
         fields = ["id", "full_name", "phone_number", "comment", "city", "product"]
 
         extra_kwargs = {

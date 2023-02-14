b1ee9a65ca33b108f8bca1359402434566111643
Adilzhan Aubakir <aubakirof@gmail.com>
2023-02-14 13:38:35 +0600

    update

--- core/utils.py
+++ core/utils.py
@@ -6,7 +6,7 @@ def send_appilcation_mail(application, email):
     dd = send_mail(
         subject="APPLICATION TO A PRODUCT OF COMPANY FROM CAR SHOP",
         message=f"""from {application.full_name} with phone number {application.phone_number} to product 
-        {application.product.name} with product code {[application.product.product_code]}""",
+        {application.product.name} with product code {application.product.product_code}""",
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[email]
     )
